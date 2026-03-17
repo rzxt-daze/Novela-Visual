cc609ac7fd951f6709a428b1c8618c322713dbb0
Elquequito <joa.lecaros@duocuc.cl>
2026-03-17 00:47:38 -0300

    primer commit proyecto visual novel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton
+  ]
 })
 export class HomePage {
-  constructor() {}
+
+  personaje: string = "...";
+  texto: string = "Sales de tu trabajo de medio tiempo y chocas con una chica al salir por la puerta.";
+
+  mostrarWaifu: boolean = false;
+  
+  siguiente() {
+    this.personaje = "Rika";
+    this.texto = "¡Ey tú! Fijate por donde caminas idiota.";
+    this.mostrarWaifu = true;
+  }
+
+  ignorar() {
+    this.personaje = "Sayori";
+    this.texto = "Oye... ¿por qué me ignoras?";
+  }
+
 }
